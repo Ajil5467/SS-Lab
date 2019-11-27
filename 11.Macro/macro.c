198c6ed19c8e3ec9de983df81805c08ad4127b37
Ajil <39672981+Ajil5467@users.noreply.github.com>
2019-11-27 05:49:59 +0530

    Update macro.c

--- 11.Macro/macro.c
+++ 11.Macro/macro.c
@@ -1,4 +1,4 @@
-//IMPLEMENTATION OF ONE PASS MACRO PROCESSOR
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -8,14 +8,14 @@ FILE *f1,*f2,*f3,*f4,*f5;
 int len,i,pos=1;
 char arg[20],mne[20],opnd[20],la[20],name[20],mne1[20],opnd1[20],pos1[10],pos2[10];
 f1=fopen(argv[1],"r");
-f2=fopen(argv[2],"w+"); //Reading the files as arguments via terminal
+f2=fopen(argv[2],"w+"); 
 f3=fopen(argv[3],"w+");
 f4=fopen(argv[4],"w+");
 f5=fopen(argv[5],"w+");
 fscanf(f1,"%s%s%s",la,mne,opnd);
 while(strcmp(mne,"END")!=0)
 {
-if(strcmp(mne,"MACRO")==0) //Checking macro as opcode
+if(strcmp(mne,"MACRO")==0) 
 {
 fprintf(f2,"%s\n",la);
 fseek(f2,SEEK_SET,0);
@@ -53,7 +53,7 @@ fseek(f4,SEEK_SET,0);
 fscanf(f3,"%s%s",mne1,opnd1);
 fprintf(f5,".\t%s\t%s\n",mne1,opnd);
 fscanf(f3,"%s%s",mne1,opnd1);
-while(strcmp(mne1,"MEND")!=0) //Checking whether it is end of macro
+while(strcmp(mne1,"MEND")!=0) 
 {
 if((opnd[0]=='?'))
 {
@@ -74,11 +74,11 @@ fprintf(f5,"%s\t%s\t%s",la,mne,opnd);
 fclose(f1);
 fclose(f2);
 fclose(f3);
-fclose(f4); //Closing files
+fclose(f4); 
 fclose(f5);
 printf("files to be viewed \n");
 printf("1. namtab.txt\n");
 printf("2. deftab.txt\n");
-printf("3. argtab.txt\n"); //Order in which files are viewed
+printf("3. argtab.txt\n");
 printf("4. op.txt\n");
 }
