eba31f2f33b4efafd3f8b78ebb1f77d892db16e1
TinkerHub GEC PKD <56582832+TinkerHubGec@users.noreply.github.com>
2020-10-08 09:43:57 -0800

    Update macro.c

--- 11.Macro/macro.c
+++ 11.Macro/macro.c
@@ -1,84 +1,124 @@
-
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-int main(int argc, char *argv[])
+
+
+struct file
 {
-FILE *f1,*f2,*f3,*f4,*f5;
-int len,i,pos=1;
-char arg[20],mne[20],opnd[20],la[20],name[20],mne1[20],opnd1[20],pos1[10],pos2[10];
-f1=fopen(argv[1],"r");
-f2=fopen(argv[2],"w+"); 
-f3=fopen(argv[3],"w+");
-f4=fopen(argv[4],"w+");
-f5=fopen(argv[5],"w+");
-fscanf(f1,"%s%s%s",la,mne,opnd);
-while(strcmp(mne,"END")!=0)
-{
-if(strcmp(mne,"MACRO")==0) 
-{
-fprintf(f2,"%s\n",la);
-fseek(f2,SEEK_SET,0);
-fprintf(f3,"%s\t%s\n",la,opnd);
-fscanf(f1,"%s%s%s",la,mne,opnd);
-while(strcmp(mne,"MEND")!=0)
-{
-if(opnd[0]=='&')
-{
-snprintf(pos1,20,"%d",pos);
-strcpy(pos2,"?");
-strcpy(opnd,strcat(pos2,pos1));
-pos=pos+1;
-}
-fprintf(f3,"%s\t%s\n",mne,opnd);
-fscanf(f1,"%s%s%s",la,mne,opnd);
-}
-fprintf(f3,"%s",mne);
-}
-else
-{
-fscanf(f2,"%s",name);
-if(strcmp(mne,name)==0)
-{
-len=strlen(opnd);
-for(i=0;i<len;i++)
-{
-if(opnd[i]!=',')
-fprintf(f4,"%c",opnd[i]);
-else
-fprintf(f4,"\n");
-}
-fseek(f3,SEEK_SET,0);
-fseek(f4,SEEK_SET,0);
-fscanf(f3,"%s%s",mne1,opnd1);
-fprintf(f5,".\t%s\t%s\n",mne1,opnd);
-fscanf(f3,"%s%s",mne1,opnd1);
-while(strcmp(mne1,"MEND")!=0) 
-{
-if((opnd[0]=='?'))
-{
-fscanf(f4,"%s",arg);
-fprintf(f5,"-\t%s\t%s\n",mne1,arg);
-}
-else
-fprintf(f5,"-\t%s\t%s\n",mne1,opnd1);
-fscanf(f3,"%s%s",mne1,opnd1);
-}
-}
-else
-fprintf(f5,"%s\t%s\t%s\n",la,mne,opnd);
-}
-fscanf(f1,"%s%s%s",la,mne,opnd);
-}
-fprintf(f5,"%s\t%s\t%s",la,mne,opnd);
-fclose(f1);
-fclose(f2);
-fclose(f3);
-fclose(f4); 
-fclose(f5);
-printf("files to be viewed \n");
-printf("1. namtab.txt\n");
-printf("2. deftab.txt\n");
-printf("3. argtab.txt\n");
-printf("4. op.txt\n");
+int all[10];
+int max[10];
+int need[10];
+int flag;
+}f[10];
+
+
+
+void main()
+{	//initialization of variables
+	int fl;
+	int i, j, k, p, b, n, r, g, cnt=0, id, newr;
+	int avail[10],seq[10];
+	printf("\nEnter number of processes: ");
+	scanf("%d",&n);
+	printf("Enter number of resources: ");
+	scanf("%d",&r);
+	
+	for(i=0;i<n;i++)
+	//Entering details of all processes
+	{
+		printf("Enter details for P%d",i);
+		printf("\nEnter allocation\t: \t");
+		for(j=0;j<r;j++)
+			scanf("%d",&f[i].all[j]);
+		printf("Enter Max\t\t: \t");
+		for(j=0;j<r;j++)
+			scanf("%d",&f[i].max[j]);
+		f[i].flag=0;
+	}
+	printf("\nEnter Available Resources\t: \t");
+	for(i=0;i<r;i++)
+		scanf("%d",&avail[i]);
+	printf("\nEnter New Request Details : ");
+	printf("\nEnter pid \t : \t");
+	scanf("%d",&id);
+	printf("Enter Request for Resources \t: \t");
+	for(i=0;i<r;i++)
+	{
+		scanf("%d",&newr);
+	f[id].all[i] += newr;
+	avail[i]=avail[i]-newr;
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<r;j++)
+		{
+			f[i].need[j] = f[i].max[j]-f[i].all[j];
+			if(f[i].need[j]<0)
+				f[i].need[j]=0;
+		}
+	}
+	cnt=0;
+	fl=0;
+	while(cnt!=n)
+	{
+		g=0;
+		for(j=0;j<n;j++)
+		{
+			if(f[j].flag==0)
+			{
+				b=0;
+				for(p=0;p<r;p++)
+				{
+					if(avail[p]>=f[j].need[p])
+						
+						b=b+1;
+					else
+						b=b-1;
+				}
+				if(b==r)
+				{
+					printf("\nP%d is visited",j);
+					
+					seq[fl++]=j;
+					f[j].flag=1;
+					for(k=0;k<r;k++)
+						avail[k]=avail[k]+f[j].all[k];
+					cnt=cnt+1;
+					printf("(");
+					for(k=0;k<r;k++)
+						printf(" %d",avail[k]);
+					printf(")");
+					g=1;
+				}
+			}
+		}
+		if(g==0)
+		//Unsafe state
+		{	printf("\n REQUEST NOT GRANTED- DEADLOCK OCCURRED");
+			printf("\n SYSTEM IS IN UNSAFE STATE");
+			goto y;
+		}
+	}
+	printf("\nSYSTEM IS IN SAFE STATE");
+	//Safe state
+	printf("\nThe Safe Sequence is : (");
+	for(i=0;i<fl;i++)
+		printf("P%d ",seq[i]);
+	printf(")\n");
+	y: printf("\n\Process\t\t\Allocation\t\t  Max\t\t  Need\n");
+	for(i=0;i<n;i++)
+	{
+		printf("P%d\t",i);
+		printf("\t");
+		for(j=0;j<r;j++)
+			printf(" %d",f[i].all[j]);
+		printf("\t\t\t");
+		
+		for(j=0;j<r;j++)
+			printf(" %d",f[i].max[j]);
+		printf("\t\t");
+		
+		for(j=0;j<r;j++)
+			printf(" %d",f[i].need[j]);
+			
+		printf("\n");
+	}
 }
