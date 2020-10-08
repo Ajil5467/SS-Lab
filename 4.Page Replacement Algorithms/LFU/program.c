a768f8b1175db62f4f8d9da32b4b0b61bc2b4924
TinkerHub GEC PKD <56582832+TinkerHubGec@users.noreply.github.com>
2020-10-08 09:45:06 -0800

    Update program.c

--- 4.Page Replacement Algorithms/LFU/program.c
+++ 4.Page Replacement Algorithms/LFU/program.c
@@ -5,81 +5,87 @@
    Since actual physical memory is much smaller than virtual memory, page faults happen. In case of page fault, Operating System might have to replace one of the existing pages with the newly needed page.
    In this algorithm, pages are replaced which would not be used for the longest duration of time in the future.
  */
-#include <stdio.h>
 
-int main()
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main(int argc, char *argv[])
 {
-        int frames[10], temp[10], pages[10];
-        int total_pages, m, n, position, k, l, total_frames;
-        int a = 0, b = 0, page_fault = 0;
-        printf("\nEnter Total Number of Frames:\t");
-        scanf("%d", &total_frames);
-        for(m = 0; m < total_frames; m++)
-        {
-                frames[m] = -1; //initialize frame with -1.
-        }
-        printf("Enter Total Number of Pages:\t");
-        scanf("%d", &total_pages);
-        printf("Enter Values for Reference String:\n");
-        for(m = 0; m < total_pages; m++)
-        {
-                printf("Value No.[%d]:\t", m + 1);
-                scanf("%d", &pages[m]);
-        }
-        for(n = 0; n < total_pages; n++)
-        {
-                a = 0, b = 0;
-                for(m = 0; m < total_frames; m++)
-                {
-                        if(frames[m] == pages[n]) //page is already in frame .HIT occured
-                        {
-                                a = 1;
-                                b = 1;
-                                break;
-                        }
-                }
-                if(a == 0) //to fill empty spaces
-                {
-                        for(m = 0; m < total_frames; m++)
-                        {
-                                if(frames[m] == -1)
-                                {
-                                        frames[m] = pages[n]; //add page to empty frame .
-                                        b = 1;
-                                        break;
-                                }
-                        }
-                }
-                if(b == 0) //if there is no empty frames then add page to top of the frame array
-                {
-                        for(m = 0; m < total_frames; m++)
-                        {
-                                temp[m] = 0;
-                        }
-                        for(k = n + 1, l = 1; l <= total_frames - 1; l++, k++) // pages in frames are compared with pages in references.
-                        {
-                                for(m = 0; m < total_frames; m++)
-                                {
-                                        if(frames[m] == pages[k])
-                                        {
-                                                temp[m] = 1; //if the page would be used in the future then temp=1
-                                        }
-                                }
-                        }
-                        for(m = 0; m < total_frames; m++)
-                        {
-                                if(temp[m] == 0)
-                                        position = m;  //this is the position ,in which this position previously have a page which would not be used in the future
-                        }
-                        frames[position] = pages[n];
-                        page_fault++;
-                }
-                printf("\n");
-                for(m = 0; m < total_frames; m++)
-                {
-                        printf("%d\t", frames[m]); //display the frames.
-                }
-        }
-        printf("\nTotal Number of Page Faults:\t%d\n", page_fault);
-        return 0;
+FILE *f1,*f2,*f3,*f4,*f5;
+int len,i,pos=1;
+char arg[20],mne[20],opnd[20],la[20],name[20],mne1[20],opnd1[20],pos1[10],pos2[10];
+f1=fopen(argv[1],"r");
+f2=fopen(argv[2],"w+"); 
+f3=fopen(argv[3],"w+");
+f4=fopen(argv[4],"w+");
+f5=fopen(argv[5],"w+");
+fscanf(f1,"%s%s%s",la,mne,opnd);
+while(strcmp(mne,"END")!=0)
+{
+if(strcmp(mne,"MACRO")==0) 
+{
+fprintf(f2,"%s\n",la);
+fseek(f2,SEEK_SET,0);
+fprintf(f3,"%s\t%s\n",la,opnd);
+fscanf(f1,"%s%s%s",la,mne,opnd);
+while(strcmp(mne,"MEND")!=0)
+{
+if(opnd[0]=='&')
+{
+snprintf(pos1,20,"%d",pos);
+strcpy(pos2,"?");
+strcpy(opnd,strcat(pos2,pos1));
+pos=pos+1;
+}
+fprintf(f3,"%s\t%s\n",mne,opnd);
+fscanf(f1,"%s%s%s",la,mne,opnd);
+}
+fprintf(f3,"%s",mne);
+}
+else
+{
+fscanf(f2,"%s",name);
+if(strcmp(mne,name)==0)
+{
+len=strlen(opnd);
+for(i=0;i<len;i++)
+{
+if(opnd[i]!=',')
+fprintf(f4,"%c",opnd[i]);
+else
+fprintf(f4,"\n");
+}
+fseek(f3,SEEK_SET,0);
+fseek(f4,SEEK_SET,0);
+fscanf(f3,"%s%s",mne1,opnd1);
+fprintf(f5,".\t%s\t%s\n",mne1,opnd);
+fscanf(f3,"%s%s",mne1,opnd1);
+while(strcmp(mne1,"MEND")!=0) 
+{
+if((opnd[0]=='?'))
+{
+fscanf(f4,"%s",arg);
+fprintf(f5,"-\t%s\t%s\n",mne1,arg);
+}
+else
+fprintf(f5,"-\t%s\t%s\n",mne1,opnd1);
+fscanf(f3,"%s%s",mne1,opnd1);
+}
+}
+else
+fprintf(f5,"%s\t%s\t%s\n",la,mne,opnd);
+}
+fscanf(f1,"%s%s%s",la,mne,opnd);
+}
+fprintf(f5,"%s\t%s\t%s",la,mne,opnd);
+fclose(f1);
+fclose(f2);
+fclose(f3);
+fclose(f4); 
+fclose(f5);
+printf("files to be viewed \n");
+printf("1. namtab.txt\n");
+printf("2. deftab.txt\n");
+printf("3. argtab.txt\n");
+printf("4. op.txt\n");
 }
