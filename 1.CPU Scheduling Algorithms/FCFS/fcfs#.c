75cf7c500f441692aacdea4a010321ae352b0f4c
Restyled.io <commits@restyled.io>
2020-10-11 08:39:01 +0000

    Restyled by astyle

--- 1.CPU Scheduling Algorithms/FCFS/fcfs#.c
+++ 1.CPU Scheduling Algorithms/FCFS/fcfs#.c
@@ -1,8 +1,8 @@
- C program for implementation of FCFS   
-// scheduling  
-#include<stdio.h>  
-// Function to find the waiting time for all   
-// processes  
+C program for implementation of FCFS
+// scheduling
+#include<stdio.h>
+// Function to find the waiting time for all
+// processes
 
-    //Display processes along with all details  
-    p
+//Display processes along with all details
+p
